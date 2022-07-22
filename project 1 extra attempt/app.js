c7fb95f5080787fef80f2842be48794e86193106
JRose117 <joerose123@hotmail.co.uk>
2022-07-22 10:51:26 +0100

    working with difficulty added and files in correct location

--- project 1 extra attempt/app.js
+++ project 1 extra attempt/app.js
@@ -1,4 +1,5 @@
 function init() {
+
   const grid = document.querySelector('.grid')
   const minesLeftHTML = document.querySelector('.minesLeftTop')
   const resetButton = document.querySelector('.restart button')
@@ -6,11 +7,11 @@ function init() {
   const timerDisplay = document.querySelector('.timer')
   const instructionsTop = document.querySelector('.levelTop')
   const instructionsBelow = document.querySelector('.levelBottom')
-  let width
-  let height
-  let mines
+
+  let width = 8
+  let height = 8
+  let mines = 10
   let timer
-  let timerCount
   let firstClick = 1
   let timerRunning = false
   let minesLeft = mines
@@ -21,29 +22,9 @@ function init() {
   let cellCount
   const cells = []
   let createMineArray = []
-  let buttonSelected
-  let looping = 1
-  let difficulty = parseFloat(prompt('Type 1 for Easy Mode or 2 for Challenge Mode'))
-  while (looping === 1) {
-    if (difficulty > 2 || difficulty < 1 || Number.isNaN(difficulty) || difficulty % 1 !== 0) {
-      console.log(difficulty)
-      difficulty = parseFloat(prompt('Enter 1 for Easy Mode or 2 for Challenge Mode'))
-    } else {
-      looping = 2
-    }
-  }
+
   function createGrid() {
-    if (difficulty === 1) {
-      width = 8
-      height = 8
-      mines = 10
-    } else if (difficulty === 2) {
-      width = 16
-      height = 16
-      mines = 40
-    }
     cellCount = width * height
-    minesLeft = mines
     const styleHeight = 100 / height
     const styleWidth = 100 / width
     for (let i = 0; i < cellCount; i++) {
@@ -53,40 +34,36 @@ function init() {
       const cell = document.createElement('button')
       cell.style.height = `${styleHeight}%`
       cell.style.width = `${styleWidth}%`
-      if (difficulty === 2) {
-        cell.style.border = '2px solid #1b7123'
-      }
+      // cell.innerText = `x:${x},y:${y},i:${i}`
       cell.setAttribute('id', 'tile')
       cell.dataset.x = x
       cell.dataset.y = y
       cell.setAttribute('value', i)
       cells.push(cell)
       grid.appendChild(cell)
-
     }
     minesLeft = mines
     minesLeftHTML.innerHTML = minesLeft
-    buttonSelected = document.querySelectorAll('#tile')
   }
   createGrid()
   function createMines(position) {
     cells[position].classList.add('mine')
   }
+
   function runGame(event) {
     if (gameRunning === 2) {
       return
-    }
+    } 
     startTimer()
     if (event.target.classList.contains('revealed')) {
       return
     } else {
       if (firstClick === 1) {
         createMineArray.push(parseFloat(event.target.value))
-        firstClick = 2
-      }
+        firstClick = 2}
       while (createMineArray.length != mines + 1) {
         const randomNumberMine = Math.floor(Math.random() * cells.length)
-        if (!createMineArray.includes(randomNumberMine || parseFloat(event.target.value))) {
+        if (!createMineArray.includes(randomNumberMine||parseFloat(event.target.value))) {
           createMineArray.push(randomNumberMine)
           createMines(randomNumberMine)
           cells[randomNumberMine].setAttribute('mine', true)
@@ -102,7 +79,8 @@ function init() {
           minesLeft += 1
           minesLeftHTML.innerHTML = minesLeft
           return
-        } else {
+        }
+        else {
           return
         }
       } else if (flag === true) {
@@ -111,17 +89,14 @@ function init() {
         minesLeft -= 1
       } else if (event.target.classList.contains('mine')) {
         cells[event.target.value].innerHTML = '💣'
+        cells[event.target.value].style.fontSize = '50px'
+        console.log('lose')
         title.innerHTML = 'YOU LOSE 😢'
-        for (let i = 0; i < cellCount; i++) {
-          if (cells[i].classList.contains('mine')) {
-            cells[i].innerText = '💣'
-          }
-        }
+
         gameRunning = 2
-        event.target.removeAttribute('mine')
-        event.target.classList.remove('mine')
         clearTimeout(timer)
         timerRunning = false
+
       } else if (event.target.classList.contains('restart')) {
         return
       } else if (event.target.classList.contains('revealed')) {
@@ -138,19 +113,17 @@ function init() {
     }
     else if (flag === true) {
       flag = false
-      flagButtonSelected.style.background = '#2a8a33'
-      flagButtonSelected.style.border = '5px solid #1b7123'
       instructionsTop.innerHTML = 'Pick'
       instructionsBelow.innerHTML = 'Tiles'
     } else {
       flag = true
-      flagButtonSelected.style.background = '#94aa5f'
-      flagButtonSelected.style.border = '1px solid #7c9444'
       instructionsTop.innerHTML = 'Place'
       instructionsBelow.innerHTML = 'Flags'
     }
+
   }
   function checkArea(event) {
+    // console.log(`line 118 gameRunning - > ${gameRunning}`)
     let x = 0
     let y = 0
     if (event.target) {
@@ -161,6 +134,7 @@ function init() {
         y = parseFloat(event.target.dataset.y)
       }
     } else {
+      console.log(`event -> ${event}`)
       if (cells[event].classList.contains('revealed') || (cells[event].classList.contains('mine'))) {
         return
       } else if (event) {
@@ -173,8 +147,13 @@ function init() {
       }
     }
     let count = 0
+    console.log(`x --> ${x}`)
+    console.log(`y --> ${y}`)
+    console.log(`width --> ${width}`)
     const iFromCoordinates = ((width * y) + x)
+    console.log(`iFromCoordinates -> ${iFromCoordinates}`)
     const left = x - 1
+    console.log(`left -> ${left}`)
     const right = x + 1
     const top = y - 1
     const below = y + 1
@@ -187,7 +166,16 @@ function init() {
     const belowCoordinate = ((width * below) + x)
     const belowRightCoordinate = ((width * below) + right)
     let coordinatesArray = [topLeftCoordinate, topCoordinate, topRightCoordinate, leftCoordinate, rightCoordinate, belowLeftCoordinate, belowCoordinate, belowRightCoordinate]
+    console.log(`[topLeftCoordinate -> ${topLeftCoordinate}, topCoordinate -> ${topCoordinate}, topRightCoordinate -> ${topRightCoordinate}, leftCoordinate -> ${leftCoordinate}, rightCoordinate -> ${rightCoordinate}, belowLeftCoordinate -> ${belowLeftCoordinate}, belowCoordinate -> ${belowCoordinate}, belowRightCoordinate -> ${belowRightCoordinate}]`)
     coordinatesArray = coordinatesArray.filter(i => cells[i])
+    console.log(coordinatesArray)
+
+
+    // const count = coordinatesArray.reduce((prev,current) => {
+    //   return cells[current].classList.contains('mine') ? prev + 1 : prev
+    // }, 0)
+    // console.log(count)
+    console.log(coordinatesArray.includes(topLeftCoordinate))
     if ((!(top < 0 || left < 0)) && cells[topLeftCoordinate].classList.contains('mine') && coordinatesArray.includes(topLeftCoordinate)) {
       count += 1
     }
@@ -210,80 +198,85 @@ function init() {
       count += 1
     }
     if ((!(below >= height || right >= width)) && cells[belowRightCoordinate].classList.contains('mine') && coordinatesArray.includes(belowRightCoordinate)) {
+      // console.log(`line 1 ${belowRightCoordinate}`)
+      // console.log(`line 2 ${(!(below >= height || right >= width))}`)
       count += 1
     }
-    if (event === 0) {
-      if (!cells[event].classList.contains('revealed')) {
-        cells[event].classList.add('revealed')
-        revealed += 1
-      }
-    }
+    // console.log(`count --> ${count}`)
+    // console.log(`event ${event}`)
     cells[iFromCoordinates].innerHTML = count
     cells[iFromCoordinates].dataset.count = count
-    if (count === 0) {
-      cells[iFromCoordinates].innerHTML = ''
-    } else if (count === 1) {
-      cells[iFromCoordinates].style.color = 'blue'
-    } else if (count === 2) {
-      cells[iFromCoordinates].style.color = 'green'
-    } else if (count === 3) {
-      cells[iFromCoordinates].style.color = 'red'
-    } else if (count === 4) {
-      cells[iFromCoordinates].style.color = 'navy'
-    } else if (count === 5) {
-      cells[iFromCoordinates].style.color = 'maroon'
-    } else if (count === 6) {
-      cells[iFromCoordinates].style.color = 'teal'
-    } else if (count === 7) {
-      cells[iFromCoordinates].style.color = 'purple'
-    } else if (count === 8) {
-      cells[iFromCoordinates].style.color = 'grey'
-    }
     if (cells[iFromCoordinates].classList.contains('flagged')) {
       cells[iFromCoordinates].classList.remove('flagged')
       minesLeft += 1
       minesLeftHTML.innerHTML = minesLeft
     }
+    console.log(`cells.length -> ${cells.length}`)
+    console.log(`mines -> ${mines}`)
+    console.log(`revealed -> ${revealed}`)
+    console.log(`cells.length-mines === -> ${cells.length-mines === revealed}`)
     if (cells.length - mines === revealed) {
+      console.log('won')
       title.innerHTML = 'YOU WON 🎉'
-      minesLeft = 0
-      minesLeftHTML.innerHTML = '0'
-      for (let i = 0; i < cellCount; i++) {
-        cells[i].innerText = '🎉'
-      }
       gameRunning = 2
       clearTimeout(timer)
       timerRunning = false
     }
+    // if (event.target){
+    // const countCheck = parseFloat(event.target.dataset.count)}
+    // else{const countCheck = parseFloat(event.target.dataset.count)}
+    // console.log(countCheck)
     if (count === 0) {
+
+
+      // console.log(`topCoordinate --> ${topCoordinate}`)
+      // console.log(`rightCoordinate --> ${rightCoordinate}`)
+      // console.log(`leftCoordinate --> ${leftCoordinate}`)
+      // console.log(`belowCoordinate --> ${belowCoordinate}`)
+      // console.log(`belowLeftCoordinate --> ${belowLeftCoordinate}`)
+      // console.log(`belowRightCoordinate --> ${belowRightCoordinate}`)
+      // console.log(`cellCount --> ${cellCount}`)
       if (!(top < 0)) {
+        // console.log(`topCoordinate --> ${topCoordinate}`)
         checkArea(topCoordinate)
       }
       if (!(left < 0)) {
+        // console.log(`leftCoordinate --> ${leftCoordinate}`)
         checkArea(leftCoordinate)
       }
       if (!(right >= width)) {
+        // console.log(`rightCoordinate --> ${rightCoordinate}`)
         checkArea(rightCoordinate)
       }
       if (!(below >= height)) {
+        // console.log(`belowCoordinate --> ${belowCoordinate}`)
         checkArea(belowCoordinate)
       }
       if (!(top < 0 || left < 0)) {
+        // console.log(`topLeftCoordinate --> ${topLeftCoordinate}`)
         checkArea(topLeftCoordinate)
       }
       if (!(top < 0 || right >= width)) {
+        // console.log(`topRightCoordinate --> ${topRightCoordinate}`)
         checkArea(topRightCoordinate)
       }
       if (!(below >= height || left < 0)) {
+        // console.log(`belowLeftCoordinate --> ${belowLeftCoordinate}`)
         checkArea(belowLeftCoordinate)
       }
       if (!(below >= height || right >= width)) {
+        // console.log(`belowRightCoordinate --> ${belowRightCoordinate}`)
         checkArea(belowRightCoordinate)
       }
     }
   }
+
+  const buttonSelected = document.querySelectorAll('#tile')
   const flagButtonSelected = document.querySelector('#flag')
   flagButtonSelected.addEventListener('click', addFlag)
+
+  buttonSelected.forEach(div => div.addEventListener('click', runGame))
+
   function reset() {
     resetTest = true
     title.innerHTML = 'MINESWEEPER'
@@ -308,12 +301,14 @@ function init() {
         cells[i].classList.remove('revealed')
       }
       cells[i].innerHTML = ''
-      cells[i].style.color = 'black'
+
+      console.log(cells[i])
+      //  resetTIMER
     }
   }
   function startTimer() {
     if (timerRunning === false) {
-      timerCount = 1
+      timerCount = 0
       clearInterval(timer)
       timer = setInterval(() => {
         timerDisplay.innerHTML = timerCount
@@ -322,7 +317,6 @@ function init() {
       timerRunning = true
     }
   }
-  buttonSelected.forEach(div => div.addEventListener('click', runGame))
 
   resetButton.addEventListener('click', reset)
 }
